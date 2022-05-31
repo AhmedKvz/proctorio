243ada111e86820c67219d755c38a82de9198c75
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2022-05-31 15:17:22 +0200

    npm package with exposed function

--- README.md
+++ README.md
@@ -3,14 +3,17 @@
 **_UNDERSTAND THE PROBLEM_**
 
 1. Can I restate the problem in my own words?
-   I have to make the new npm package, already have the name and the version. Also, there should be exported function with a list of all employees and their salaries ascendig or descending based on a salary/
-   I have to refactor the existing code and develop npm module locally / I have to make a test script to run it locally
+   I have to make the new npm package, already have the name and the version. I will not use any json server, I think it's better to do deep copy of json object, so there is no risk of altering json file
+   Use async function to retrive data from JSON.
+   Also, there should be exported function with a list of all employees and their salaries ascendig or descending based on a salary/
+   I have to refactor the existing code and develop npm module locally / I have to make a test script to run it locally.
 
 2. # What are the inputs that go into the problem?
 
    Domain, new promise, resolve, reject, getSalaries, getEmployees
 
 3. # What are the outputs that should come from the solution to the problem?
+   json server up and runing
    Package returning list of employees with their salaries
 
 **_BREAK IT DOWN_**
@@ -27,10 +30,12 @@
 
 5. index.txt convert to normal js file
 
-6. make localhost domain
+6. refactor the code of index.js, and make sure to export the function you need to export
 
-7. refactor the code of index.js and make sure to export the function you need to export
+7. index.html file for testing import type:module
 
 **Solve or simplify**
+
+# package test locally solved with npm link and another test script
 
 **Refactor**
--- src/index.js
+++ src/index.js
@@ -1,32 +1,61 @@
-var domain = null;
+const fetch = require("node-fetch");
+const employees = require("./fake-server/employees.json");
+const salaries = require("./fake-server/salaries.json");
 
-function getEmplFrom(callback) {
-  return new Promise((reject, resolve) => {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", domain + "/employees.json");
-    xhr.onload = function (e) {
-      if (this.status == 200) {
-        reject(e);
-        callback(e);
-      }
+const getEmplFrom = async () => {
+  try {
+    const data = JSON.parse(JSON.stringify(employees));
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-      resolve(e);
-    };
-
-    xhr.send();
-  });
-}
-
-function getSalaries() {
+const getSalaries = async () => {
   //TODO returns salaries from json
-  console.log("get salaries");
-}
+  const data = JSON.parse(JSON.stringify(salaries));
 
-function getEmployees(name) {
+  return data;
+};
+getSalaries();
+
+const getEmployees = async (asc = true, nullAs = Number.MAX_VALUE) => {
   //TODO should be exposed function which returns array of employees => for example [{id:"id", "name":"test", "salary":""}]
-  console.log(`hello employee ${name}`);
-}
+  try {
+    const employees = await getEmplFrom();
+    const salaries = await getSalaries();
 
-console.log("hello from my package");
+    const mappedEmloyees = employees.map((employee) => {
+      const employeeSalary = salaries.find(
+        (salary) => salary.employeeId === employee.id
+      );
+      return {
+        id: employee.id,
+        name: employee.name,
+        salary: employeeSalary.salary,
+      };
+    });
 
-module.exports = getEmployees;
+    const sortedEmployees = mappedEmloyees.sort((a, b) => {
+      let aValue = isNaN(a.salary) ? nullAs : a.salary;
+      let bValue = isNaN(b.salary) ? nullAs : b.salary;
+
+      aValue = isNaN(a.salary) ? 0 : aValue;
+      bValue = isNaN(b.salary) ? 0 : bValue;
+      const diff = aValue - bValue;
+
+      if (asc) {
+        return bValue - aValue;
+      } else {
+        return asc ? diff : -diff;
+      }
+    });
+    return sortedEmployees;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getEmployees().then((data) => console.log(data));
+
+module.exports = { getEmployees };
